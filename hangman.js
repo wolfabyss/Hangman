f73ad9661db2ad1da9cf57634a31a6433812ef3b
Alex Rasolko <arasolko@yahoo.com>
2017-08-31 20:15:01 -0700

    Added button click and disable

--- hangman.js
+++ hangman.js
@@ -33,9 +33,39 @@ let Hangman = (function(){ //this part is the iife part (google iife)
        Hangman.wordToGuess = wordToGuess.split("");
        Hangman.mask = Hangman.wordToGuess.map(()=>"*");
        Hangman.guesses = 0;
-       console.log(Hangman.wordToGuess,Hangman.mask);
-   }
+       Hangman.setupBoard();
    
+   }
+   Hangman.setupBoard = function(){
+       Hangman.board.innerHTML = Hangman.boardTemplate();
+      let guessButtons = Hangman.board.querySelectorAll(".guess-button");
+   for(let i = 0; i < guessButtons.length; i++){
+       let guessButton = guessButtons[i];
+       guessButton.addEventListener("click", Hangman.guessButtonCallback);
+   }
+}
+
+Hangman.guessButtonCallback = function(event){
+    let button = event.target;
+    let guess = button.dataset.value;
+    button.disabled = true;
+    button.classList.add('guessed');
+    console.log(guess);
+}   
+    
+   Hangman.boardTemplate = function(){
+       return `
+       <div id="guesses">${Hangman.guesses}</div>
+       <div id="mask">${Hangman.mask.join('')}</div>
+       <div id=input">${Hangman.buttonTemplate()}</div>
+       `;
+   }
+   Hangman.buttonTemplate = function(){
+    let alphabet = "abcdefghijklmnopqrstuvwxyz".split("");   
+    let alphabetButton = alphabet.map(letter=>`<button class="guess-button" data-value="${letter}">${letter}</button>`);
+return alphabetButton.join('');
+    return "";
+   }
     return Hangman;
 
 }());
